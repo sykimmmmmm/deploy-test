42b101bcbdc48b5105f6c7c365ccb7dd456bf81b
novice <seungyeon5374@gmail.com>
2024-06-17 09:49:27 +0900

    환경변수 설정 테스트

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      vercel 호스팅사이트에 배포테스트입니다.
+      <p>vercel 호스팅사이트에 배포테스트입니다.</p>
+
+      <p>버셀 환경변수 설정 테스트 : {process.env.TEST_KEY}</p>
     </div>
   );
 }
