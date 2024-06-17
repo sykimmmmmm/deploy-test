4f956c919a98aff4f53673190714a4bcd9537fb1
novice <seungyeon5374@gmail.com>
2024-06-17 09:53:15 +0900

    환경변수 설정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env.REACT_APP_TEST_KEY)
   return (
     <div className="App">
       <p>vercel 호스팅사이트에 배포테스트입니다.</p>
 
-      <p>버셀 환경변수 설정 테스트 : {process.env.TEST_KEY}</p>
+      <p>버셀 환경변수 설정 테스트 : {process.env.REACT_APP_TEST_KEY}</p>
     </div>
   );
 }
