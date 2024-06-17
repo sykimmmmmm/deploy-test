34defb000a5801aae136ac51ef614a9abdc6a882
novice <seungyeon5374@gmail.com>
2024-06-17 09:46:40 +0900

    vercel 배포 테스트

--- src/App.js
+++ src/App.js
@@ -4,20 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      vercel 호스팅사이트에 배포테스트입니다.
     </div>
   );
 }
